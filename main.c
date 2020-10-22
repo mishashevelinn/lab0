b64596115ea87b00ba3f18d16fdc44ff5b97fbdb
Michael Shevelin <shevel55505@gmail.com>
2020-10-22 23:37:42 +0300

    reformat code

--- main.c
+++ main.c
@@ -13,7 +13,7 @@ struct intl_passenger {
 
 typedef struct intl_passenger *passenger;
 
-void* cpy_elm(void *data) {
+void *cpy_elm(void *data) {
     passenger res = (passenger) malloc(sizeof(struct intl_passenger));
     passenger temp = (passenger) data;
     strcpy(res->passport_id, temp->passport_id);
@@ -23,15 +23,14 @@ void* cpy_elm(void *data) {
     return res;
 }
 
-void free_elm(void* data){
-        free(data);
+void free_elm(void *data) {
+    free(data);
 }
 
 
-
-char* label_elm(void* data){
+char *label_elm(void *data) {
     passenger temp = (passenger) data;
-    char* res = malloc(16*4+10);
+    char *res = malloc(16 * 4 + 10);
     strcpy(res, temp->given_name);
     strcat(res, " ");
     strcat(res, temp->surname);
@@ -41,27 +40,28 @@ char* label_elm(void* data){
     return res;
 }
 
-void check(){
+void check() {
     struct intl_passenger temp;
 
 
-    strcpy(temp.passport_id , "206233033");
-    strcpy(temp.given_name , "TAL");
-    strcpy(temp.surname , "SHAMIR");
-    strcpy(temp.country , "ISRAEL");
+    strcpy(temp.passport_id, "206233033");
+    strcpy(temp.given_name, "TAL");
+    strcpy(temp.surname, "SHAMIR");
+    strcpy(temp.country, "ISRAEL");
     printf("%s", label_elm(&temp));
     passenger temp1 = cpy_elm(&temp);
     printf("%s", label_elm(temp1));
     free_elm(temp1);
 }
 
-void fill_stack(Stack stack, const char* path){
+void fill_stack(Stack stack, const char *path) {
     FILE *fptr = fopen(path, "r");
     passenger temp = malloc(sizeof(struct intl_passenger));
     size_t len;
-    while ((len = (size_t) fscanf(fptr, "%s %s %s %s", temp->given_name, temp->surname, temp->passport_id, temp->country)) != EOF) {
+    while ((len = (size_t) fscanf(fptr, "%s %s %s %s", temp->given_name, temp->surname, temp->passport_id,
+                                  temp->country)) != EOF) {
         push(stack, temp);
-        }
+    }
     fclose(fptr);
     free(temp);
 }
@@ -69,9 +69,9 @@ void fill_stack(Stack stack, const char* path){
 
 int main() {
     Stack new_stack = init(56, cpy_elm, free_elm, label_elm);
-    fill_stack(new_stack, "international_passengers.dat"); 
+    fill_stack(new_stack, "international_passengers.dat");
     printf("-------STACK TEST FOR PASSENGERS DATA---------\n\n");
     print(new_stack);
-    
+
     return 0;
 }
